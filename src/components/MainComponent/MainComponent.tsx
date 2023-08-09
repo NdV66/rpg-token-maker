c25782f5352f723cb21b75107875289f14e6cd01
Marta Zazlak <marta.zazlak@hotmail.com>
2023-08-09 14:10:47 +0200

    backup

--- src/components/MainComponent/MainComponent.tsx
+++ src/components/MainComponent/MainComponent.tsx
@@ -1,3 +1,5 @@
+import { useRef } from 'react';
+
 import { IMainComponentViewModel } from 'viewModels';
 import { AvatarFrameComponent } from './AvatarFrameComponent';
 import { AvatarImageComponent } from './AvatarImageComponent';
@@ -7,22 +9,27 @@ type Props = {
 };
 
 export const MainComponent = ({ mainComponentViewModel }: Props) => {
+  const mainCanvasRef = useRef<HTMLCanvasElement>(null);
+  const frameCanvasRef = useRef<HTMLCanvasElement>(null);
+
   return (
-    <div
-      className="area"
-      onMouseLeave={() => {
-        mainComponentViewModel.imageOnCanvasMoveViewModel.turnOffIsMouseDown();
-      }}
-    >
-      <AvatarImageComponent
-        defaultWidth={mainComponentViewModel.defaultAvatarImageWidth}
-        drawImageViewModel={mainComponentViewModel.drawImageOnCanvasViewModel}
-        moveImageViewModel={mainComponentViewModel.imageOnCanvasMoveViewModel}
-      />
-      <AvatarFrameComponent
-        size={mainComponentViewModel.avatarFrameSize}
-        viewModel={mainComponentViewModel.drawImageOnCanvasViewModel}
-      />
-    </div>
+    <>
+      <div
+        className="area"
+        onMouseLeave={() => {
+          mainComponentViewModel.imageOnCanvasMoveViewModel.turnOffIsMouseDown();
+        }}
+      >
+        <AvatarImageComponent
+          defaultWidth={mainComponentViewModel.defaultAvatarImageWidth}
+          drawImageViewModel={mainComponentViewModel.drawImageOnCanvasViewModel}
+          moveImageViewModel={mainComponentViewModel.imageOnCanvasMoveViewModel}
+        />
+        <AvatarFrameComponent
+          size={mainComponentViewModel.avatarFrameSize}
+          viewModel={mainComponentViewModel.drawImageOnCanvasViewModel}
+        />
+      </div>
+    </>
   );
 };
