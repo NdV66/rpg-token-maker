feb7d86c9150dcea65244f76c5bf3a4cf39c030c
Marta Zazlak <marta.zazlak@hotmail.com>
2023-10-06 21:40:17 +0200

    test

--- src/models/ExportCanvasModel.ts
+++ src/models/ExportCanvasModel.ts
@@ -4,17 +4,11 @@ import { IImageLoaderModel } from 'models';
 import frameMask from 'data/frameMask.png';
 
 export interface IExportCanvasModel {
-  exportToPng: (
-    currentImage: HTMLCanvasElement,
-    currentFrame: HTMLCanvasElement,
-  ) => Promise<string | undefined>;
+  exportToPng: (currentImage: HTMLCanvasElement, currentFrame: HTMLCanvasElement) => Promise<string | undefined>;
 }
 
 export class ExportCanvasModel implements IExportCanvasModel {
-  constructor(
-    private readonly _appEnv: TAppEnv,
-    private readonly _imageLoader: IImageLoaderModel,
-  ) {}
+  constructor(private readonly _appEnv: TAppEnv, private readonly _imageLoader: IImageLoaderModel) {}
 
   private async _drawTokenMask(context: CanvasRenderingContext2D, frameMaskSrc: string) {
     const image = await this._imageLoader.loadImage(frameMaskSrc);
@@ -24,11 +18,7 @@ export class ExportCanvasModel implements IExportCanvasModel {
     context.restore();
   }
 
-  private _mergeCanvasLayers(
-    mergedLayersContext: CanvasRenderingContext2D,
-    currentImage: HTMLCanvasElement,
-    currentFrame: HTMLCanvasElement,
-  ) {
+  private _mergeCanvasLayers(mergedLayersContext: CanvasRenderingContext2D, currentImage: HTMLCanvasElement, currentFrame: HTMLCanvasElement) {
     mergedLayersContext.drawImage(currentImage, currentImage.offsetLeft, currentImage.offsetTop);
     mergedLayersContext.drawImage(currentFrame, currentFrame.offsetLeft, currentFrame.offsetTop);
   }
@@ -63,17 +53,7 @@ export class ExportCanvasModel implements IExportCanvasModel {
     if (mergedLayersContext && finalContext) {
       this._mergeCanvasLayers(mergedLayersContext, currentImage, currentFrame);
 
-      finalContext.drawImage(
-        mergedLayersCanvas,
-        currentFrame.offsetLeft,
-        currentFrame.offsetTop,
-        FRAME_SIZE,
-        FRAME_SIZE,
-        0,
-        0,
-        FRAME_SIZE,
-        FRAME_SIZE,
-      );
+      finalContext.drawImage(mergedLayersCanvas, currentFrame.offsetLeft, currentFrame.offsetTop, FRAME_SIZE, FRAME_SIZE, 0, 0, FRAME_SIZE, FRAME_SIZE);
       await this._drawTokenMask(finalContext, frameMask);
 
       return finalCanvas.toDataURL('image/png', 1.0);
